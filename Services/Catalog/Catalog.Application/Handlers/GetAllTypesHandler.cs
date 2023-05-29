a4f04ceac2eb9c84f821fb6848e613aafaee72ab
Omar Torres <jtorres@axsistec.com>
2023-05-29 17:47:29 -0600

    GetProductById

---
+++ Services/Catalog/Catalog.Application/Handlers/GetAllTypesHandler.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetAllTypesHandler : IRequestHandler<GetAllTypesQuery, IList<TypesResponse>>
+    {
+        private readonly ITypesRepository _typeRepository;
+
+        public GetAllTypesHandler(ITypesRepository typeRepository)
+        {
+            _typeRepository = typeRepository;
+        }
+        public async Task<IList<TypesResponse>> Handle(GetAllTypesQuery request, CancellationToken cancellationToken)
+        {
+            var typeList = await _typeRepository.GetAllTypes();
+            var typesResponseList = ProductMapper.Mapper.Map<IList<TypesResponse>>(typeList);
+            return typesResponseList;
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.Application/Responses/ProductResponse.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Application.Responses
+{
+    public class ProductResponse
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+        [BsonElement("Name")]
+        public string Name { get; set; } = string.Empty;
+        public string Summart { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageFile { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public ProductBrand Brands { get; set; }= new ProductBrand();
+        public ProductType Types { get; set; } = new ProductType();
+    }
+}
