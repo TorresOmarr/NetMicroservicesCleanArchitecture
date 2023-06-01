bbee02aaf8f70e31fdc195ffd043ee41fa073f90
Omar Torres <jtorres@axsistec.com>
2023-06-01 08:58:23 -0600

    adding product mapping

--- Services/Catalog/Catalog.Application/Mappers/ProductMappingProfile.cs
+++ Services/Catalog/Catalog.Application/Mappers/ProductMappingProfile.cs
@@ -1,11 +1,7 @@
 ﻿using AutoMapper;
+using Catalog.Application.Commands;
 using Catalog.Application.Responses;
 using Catalog.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Catalog.Application.Mappers
 {
@@ -13,7 +9,11 @@ namespace Catalog.Application.Mappers
     {
         public ProductMappingProfile()
         {
+            CreateMap<Product, ProductResponse>().ReverseMap(); 
+            CreateMap<Product, CreateProductCommand>().ReverseMap();
             CreateMap<ProductBrand, BrandResponse>().ReverseMap();  
+            CreateMap<ProductType, TypesResponse>().ReverseMap();
+
         }
     }
 }
