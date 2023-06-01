b4200791fa8df3ec2da944d72f03c5c3e2cf01f5
Omar Torres <jtorres@axsistec.com>
2023-06-01 08:18:29 -0600

    Get Product by brad Name

---
+++ Services/Catalog/Catalog.Application/Queries/GetProductByBrandQuery.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.Runtime.Internal;
+using Catalog.Application.Responses;
+using MediatR;
+
+namespace Catalog.Application.Queries
+{
+    public class GetProductByBrandQuery : IRequest<IList<ProductResponse>>
+    {
+        public string Brandname { get; set; }
+
+        public GetProductByBrandQuery(string brandname)
+        {
+            Brandname = brandname;
+        }
+    }
+}
