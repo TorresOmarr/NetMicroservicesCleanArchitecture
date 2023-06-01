b2b33e1be9dced4186b887a8e2365af9d6f0fdcb
Omar Torres <jtorres@axsistec.com>
2023-06-01 08:47:29 -0600

    create, update and delete product

---
+++ Services/Catalog/Catalog.Application/Commands/UpdateProductCommand.cs
@@ -0,0 +1,28 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Runtime.Internal;
+using MediatR;
+
+namespace Catalog.Application.Commands
+{
+    public class UpdateProductCommand : IRequest<bool>
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = null!;
+        [BsonElement("Name")]
+        public string Name { get; set; } = string.Empty;
+        public string Summary { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageFile { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public ProductBrand Brands { get; set; }
+        public ProductType Types { get; set; }
+    }
+}
