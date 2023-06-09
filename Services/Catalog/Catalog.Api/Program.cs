dac12df681616069e589634650de7b29277cd095
Omar Torres <jtorres@axsistec.com>
2023-06-09 08:50:13 -0600

    endpoint controller

--- Services/Catalog/Catalog.Api/Program.cs
+++ Services/Catalog/Catalog.Api/Program.cs
@@ -1,13 +1,32 @@
+using Catalog.Application.Commands;
+using Catalog.Application.Handlers;
+using Catalog.Infraestructure.Data.Extensions;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+IConfiguration Configuration = builder.Configuration;
+
+
 builder.Services.AddControllers();
-
-
-//// Add services to the container.
-
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddApiVersioning();
+builder.Services.AddHealthChecks()
+                .AddMongoDb(Configuration["DatabaseSettings:ConnectionString"], "Catalog MongoDb Health Check",
+                    HealthStatus.Degraded);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo(){ Title = "Catalog .API", Version = "v1" });
+});
 
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblies(typeof(CreateProductHandler).Assembly));
+
+builder.Services.AddInjectionInfrestructure(Configuration);
 
 var app = builder.Build();
 app.MapGet("/", () => "Hello World");
@@ -15,16 +34,21 @@ app.MapGet("/", () => "Hello World");
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API"));
     app.UseDeveloperExceptionPage();
 }
 
-app.UseRouting();   
-app.UseStaticFiles();   
+app.UseRouting();
+app.UseStaticFiles();
 app.UseAuthorization();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
+    endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+    {
+        Predicate = _ => true,
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 });
 
 app.Run();
---
+++ Services/Catalog/Catalog.Infraestructure/Data/Extensions/InjectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Core.Repositories;
+using Catalog.Infraestructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog.Infraestructure.Data.Extensions
+{
+    public static class InjectionExtensions
+    {
+        public static IServiceCollection AddInjectionInfrestructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ICatalogContext, CatalogContext>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IBrandRepository, ProductRepository>();
+            services.AddScoped<ITypesRepository, ProductRepository>();
+            return services;
+        }
+    }
+}
